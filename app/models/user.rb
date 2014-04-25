de1b84b51518d88039ac059da49b7cedfc4df249
Krzysztof Koziol <krzysztof.koziol@roche.com>
2014-04-25 13:40:54 +0200

    added additional check in error handling

--- app/models/user.rb
+++ app/models/user.rb
@@ -49,10 +49,12 @@ class User < ActiveRecord::Base
 
   def synchronize_devices
     devices = client.devices
-    if !devices.is_a?(Array) or devices[0].include? "error"
-      puts "Skipping sync, the following error occurred: #{devices}"
-    else
-      update_attribute(:last_sync_date, DateTime.strptime(devices.first['lastSyncTime'], "%Y-%m-%dT%T.%L")) unless devices.blank?
+    if devices
+      if !devices.is_a?(Array) or devices[0].include? "error"
+        puts "Skipping sync, the following error occurred: #{devices}"
+      else
+        update_attribute(:last_sync_date, DateTime.strptime(devices.first['lastSyncTime'], "%Y-%m-%dT%T.%L")) unless devices.blank?
+      end
     end
   end
 
